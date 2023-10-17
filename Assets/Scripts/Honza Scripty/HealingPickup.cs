2841c2e0841035f6d9a02536eda11b50b3c700bb
Jendimir <honzik4004@seznam.cz>
2023-10-17 11:15:42 +0200

    Village Fixed

---
+++ Assets/Scripts/Honza Scripty/HealingPickup.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class HealingPickup : MonoBehaviour
+{
+    public float healingAmount = 50f; // The amount of health to heal when picked up
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHP playerHealth = other.GetComponent<PlayerHP>();
+
+            if (playerHealth != null)
+            {
+                // Increase player's health
+                playerHealth.health += healingAmount;
+
+                // Clamp the player's health to a maximum value if needed
+                // playerHealth.health = Mathf.Clamp(playerHealth.health, 0f, maxHealth);
+
+                // Destroy the healing pickup object after it's picked up
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Honza Scripty/HealthBar.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider; // Reference to the UI Slider representing the health bar
+    public PlayerHP playerHP; // Reference to the PlayerHP script
+
+    void Update()
+    {
+        // Ensure the healthSlider and playerHP references are not null
+        if (healthSlider != null && playerHP != null)
+        {
+            // Update the slider value based on the player's health
+            healthSlider.value = MapHealthToSliderValue(playerHP.health);
+        }
+    }
+
+    // Map player's health to slider value (assuming health ranges from 0 to 100)
+    float MapHealthToSliderValue(float health)
+    {
+        return Mathf.Clamp01(health / 100f);
+    }
+}
