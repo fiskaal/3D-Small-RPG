b32c2bfec31ddaab9cdcd576b6a43625395118cd
Ondřej Čermák <cermaak.ondrej@gmail.com>
2024-01-16 00:47:31 +0100

    SwordDraw Audio Added

--- Assets/Prototyp/Player/Audio/PlayerAudioScript.cs
+++ Assets/Prototyp/Player/Audio/PlayerAudioScript.cs
@@ -16,6 +16,8 @@ public class PlayerAudioScript : MonoBehaviour
     public AudioClip blockHit;
     [FormerlySerializedAs("blocBreak")] public AudioClip blockBreak;
 
+    public AudioClip[] swordDraw;
+    
     public AudioClip slash0;
     public AudioClip slash1;
     public AudioClip slash2;
@@ -125,6 +127,26 @@ public class PlayerAudioScript : MonoBehaviour
         audioSource.clip = slash2Impact;
         audioSource.volume = 0.4f;
         audioSource.Play();
+    }
+    
+    public void PlaySwordDraw()
+    {
+        // Check if there is at least one AudioClip in the array
+        if (swordDraw.Length > 0)
+        {
+            // Select a random index from the stoneFootSteps array
+            int randomIndex = Random.Range(0, swordDraw.Length);
+
+            // Get the AudioClip at the random index
+            AudioClip randomAu = swordDraw[randomIndex];
+
+            // Assign the selected AudioClip to the AudioSource
+            audioSource.clip = randomAu;
+
+            audioSource.volume = 0.4f;
+            // Play the assigned AudioClip
+            audioSource.Play();
+        }
     }
 
     private void PlayRandomStoneFootStep()
