37c97159d5c9c3492c09ee0363c8a66f95feb3a1
Ondřej Čermák <cermaak.ondrej@gmail.com>
2024-01-22 18:34:05 +0100

    Canvas Player
    Všechno jsem nechal kromě playerCanvas

--- Assets/Scripts/Honza Scripty/LoadSceneOnCollision.cs
+++ Assets/Scripts/Honza Scripty/LoadSceneOnCollision.cs
@@ -1,17 +1,10 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
-using UnityEngine.UI;
-using Unity.UI;
-using System;
 
 public class LoadSceneOnCollision : MonoBehaviour
 {
     // The name of the scene you want to load
     public string sceneName = "TUTORIAL_level";
-    public GameObject LoadingPage;
-    public Slider slider;
 
     // The spawn coordinates for the player
     public Vector3 spawnCoordinates;
@@ -26,32 +19,7 @@ public class LoadSceneOnCollision : MonoBehaviour
             other.transform.position = spawnCoordinates;
 
             // Load the specified scene
-            //SceneManager.LoadScene(sceneName);
-
-            StartCoroutine(LoadAsynchro(sceneName));
+            SceneManager.LoadScene(sceneName);
         }
-    }
-
-    public void LoadLevel(string nameOfTheScene)
-    {
-        StartCoroutine(LoadAsynchro(nameOfTheScene));
-
-    }
-
-    IEnumerator LoadAsynchro(string nameOfTheScene)
-    {
-        AsyncOperation operation = SceneManager.LoadSceneAsync(nameOfTheScene);
-
-        LoadingPage.SetActive(true);
-
-        while (!operation.isDone)
-        {
-
-            float progress = Mathf.Clamp01(operation.progress / .9f);
-            Debug.Log(progress);
-            slider.value = progress;
-            yield return null;
-        }
-
     }
 }
